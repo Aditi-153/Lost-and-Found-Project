f319793b762413c99a0f425b84522860696c87fd
Aditi-153 <pachpatiladiti@gmail.com>
2026-01-23 15:38:14 +0530

    initial setup

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 import express from 'express';
-const app = express();
-import path from "path"
+import dotenv from "dotenv";
 
+import cookieParser from 'cookie-parser';
+import mongoose from "mongoose";
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
