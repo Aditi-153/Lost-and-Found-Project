a8a4765821abbc1469b412901be80bd1a6634271
Aditi-153 <pachpatiladiti@gmail.com>
2026-01-25 22:22:03 +0530

    Add Bun lockfile for dependency management

--- controllers/user.js
+++ controllers/user.js
@@ -111,3 +111,6 @@ export const login = async (req, res) => {
     });
   }
 };
+
+
+
---
+++ models/listing.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import User from "./user.js";
+
+export const ListingSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    
+    },
+
+    descriptionArr : {
+        type : Array,
+        required : true
+    },
+
+    img : {
+        type : String, //url
+    },
+
+    location : {
+        type : String,
+        required : true
+    },
+
+    status : {
+        type : String ,
+        enum : ["Lost" , "Found"]
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+
+    Author: [{ 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User" 
+    }],
+
+
+
+
+
+})
+
+module.exports = mongoose.model("Listing" , ListingSchema)
